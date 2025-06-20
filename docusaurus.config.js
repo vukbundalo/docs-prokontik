e794905f696869a920cf640e6d2d24dde3086f5c
Vuk Bundalo <vukbundalo777@gmail.com>
2025-06-21 00:58:48 +0200

    Fix projectName to docs-prokontik

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'vukbundalo', // Usually your GitHub org/user name.
-  projectName: 'prokontik_docs', // Usually your repo name.
+  projectName: 'docs-prokontik', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
